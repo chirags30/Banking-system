9b4d7d115250158e5f713cdc6bd8e88245c958d0
chirags30 <59804933+chirags30@users.noreply.github.com>
2021-07-18 08:57:25 +0530

    Add files via upload

---
+++ BankingSystem/footer.php
@@ -0,0 +1,35 @@
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+  
+    <!-- Section: Social media -->
+    
+    <section class="mb-4">
+    <div class="text-center p-3" >
+   Created by Chirag Sharma:
+    <!-- Linkedin -->
+   <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fab fa-linkedin-in"></i
+      >Linkedin</a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/chirags30" role="button"
+        ><i class="fab fa-github"></i
+
+      > Github</a>
+   
+  </div>
+     
+
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://www.thesparksfoundationsingapore.org/">The Spark Foundation</a>
+  </div>
+  <!-- Copyright -->
+</footer>
